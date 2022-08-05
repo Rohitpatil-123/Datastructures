4eb59cf6b7f37bdb205b0dbf5d02408c54542d45
ROHIT SANJAY PATIL <99815954+Rohitpatil-123@users.noreply.github.com>
2022-08-05 20:56:01 +0530

    Add files via upload

---
+++ rem_duplicate_in_sorted_LL.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practice;
+
+/**
+ *
+ * @author RR
+ */
+
+//this program is for removing duplicate elements from the linked list
+//assume this linked to be sorted
+
+public class rem_duplicate_in_sorted_LL {
+     Node head=null;
+    class Node{
+        int data;
+        Node next;
+        
+        public Node(int data){
+            this.data=data;
+            this.next=null;
+            
+        }       
+    }
+    public void create(int data){
+        Node new_node=new Node(data);
+        Node curr=head;
+        if(head==null){
+            head=new_node;        
+        }
+        else{
+            
+            while(curr.next!=null){
+                curr=curr.next;
+                
+            }
+            curr.next=new_node;
+        }
+    }
+      public void print(){
+        Node curr=head;
+        while(curr!=null){
+            System.out.print(curr.data+"-->");
+            curr=curr.next;
+        }
+        System.out.println("null");
+    }
+     public void duplicate(){
+        Node curr=head;
+        
+        while(curr.next!=null){
+            if(curr.data==curr.next.data){
+                curr.next=curr.next.next;
+                
+            }
+            else{
+                curr=curr.next;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        mid_of_LL h=new mid_of_LL();
+        h.create(1);
+        h.create(1);
+        h.create(2);
+        h.create(2);
+        h.create(3);
+        h.create(3);
+        h.create(3);
+        h.print();
+      
+        h.duplicate();
+      
+        h.print();
+    }
+    
+    
+}
